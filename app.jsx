392b488b57aab36485adf6f7e6169a7e5d8f356a
bhavyakunchala2006-debug <bhavyakunchala2006@gmail.com>
2026-01-08 12:47:24 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,9 @@
+import Dashboard from "./pages/Dashboard";
+
+export default function App() {
+  return (
+    <div className="flex h-screen overflow-hidden bg-background">
+      <Dashboard />
+    </div>
+  );
+)
